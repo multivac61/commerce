b64d7ee91fa5137845db94d3cf868911c1482701
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-05-07 15:40:24 +0000

    Add types

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,6 @@
-export const createUrl = (pathname: string, params: URLSearchParams) => {
+import { ReadonlyURLSearchParams } from 'next/navigation';
+
+export const createUrl = (pathname: string, params: ReadonlyURLSearchParams | URLSearchParams) => {
   const paramsString = params.toString();
   const queryString = `${paramsString.length ? '?' : ''}${paramsString}`;
 
